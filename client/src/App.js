41e1b874ad23117734e3d8dd0390a78966bae097
@nkit <ankit.mene@wovvtech.com>
2023-03-25 12:47:09 +0530

    crud changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,106 @@
-
-
-import "./App.css"
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  const [responseData, setresponseData] = useState("");
+  const [upadteData, setupadteData] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      console.log(response);
+      setresponseData(response.data);
+    });
+  }, []);
+  const submitReview = () => {
+    // alert("data is here", movieName, review);
+    console.log("data", movieName, review);
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    }).then(() => {
+      setresponseData([
+        ...responseData,
+        { movieName: movieName, movieReview: review },
+      ]);
+    });
+  };
+
+  const deleteReview = (name) => {
+    console.log("movie name", name);
+    Axios.delete(`http://localhost:3001/api/delete/${name}`).then(() => {});
+  };
+
+  const updateReview = (name) => {
+    console.log("movie name", upadteData);
+    Axios.put("http://localhost:3001/api/update", {
+      name: name,
+      upadteData: upadteData,
+    }).then(() => {});
+  };
+  const [selectedValue, setSelectedValue] = useState("option1");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
   return (
     <div className="App">
       <h1> CRUD OPERATIONS </h1>
       <div className="form">
-        
-<label>Movie Name</label>
-        <input type="text" name="movie name" /> 
+        <label>Movie Name</label>
+        <input
+          type="text"
+          name="movie name"
+          onChange={(e) => setMovieName(e.target.value)}
+        />
         <label>Review</label>
-        <input type="text" name="review" /> 
-         <button> Submit</button>
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => setReview(e.target.value)}
+        />
+        <button onClick={submitReview}> Submit</button>
+        {responseData &&
+          responseData.map((val) => {
+            return (
+              <div className="card">
+                <h1>Movie Name: {val.movieName} </h1>
+                <p> Movie Review: {val.movieReview}</p>
+
+                <button
+                  onClick={() => {
+                    deleteReview(val.movieName);
+                  }}
+                >
+                  Delete
+                </button>
+                <input
+                  type="text"
+                  id="updateInput"
+                  onChange={(e) => setupadteData(e.target.value)}
+                />
+                <button
+                  onClick={() => {
+                    updateReview(val.movieName);
+                  }}
+                >
+                  Update
+                </button>
+
+                <select
+                  id="my-dropdown"
+                  value={selectedValue}
+                  onChange={handleChange}
+                >
+                  <option value="option1">Option 1</option>
+                  <option value="option2">Option 2</option>
+                  <option value="option3">Option 3</option>
+                </select>
+              </div>
+            );
+          })}
       </div>
-     
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,55 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql");
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hii ANkit ")
-})
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "ammene27",
+  database: "CRUDDataBase",
+});
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM movie_reviews";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.delete("/api/delete/:name", (req, res) => {
+  const mName = req.params.name;
+  console.log("movie name delete", mName);
+
+  const sqlDelete = "DELETE FROM movie_reviews WHERE movieName=?";
+  db.query(sqlDelete, mName, (err, result) => {
+    console.log(err);
+  });
+});
+
+app.put("/api/update", (req, res) => {
+  const upadteData = req.body.upadteData;
+  const mName = req.body.name;
+  console.log("movie name delete", mName);
+
+  const sqlPut = "UPDATE movie_reviews SET  movieReview = ? WHERE movieName=?";
+  db.query(sqlPut, [upadteData, mName], (err, result) => {
+    console.log(err);
+  });
+});
+app.post("/api/insert", (req, res) => {
+  const mName = req.body.movieName;
+  const mReview = req.body.movieReview;
+  console.log("data in server", mName, mReview);
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+  db.query(sqlInsert, [mName, mReview], (err, result) => {});
+});
 
 app.listen(3001, () => {
-    console.log("Running on server 3001")
-})
+  console.log("Running on server 3001");
+});
